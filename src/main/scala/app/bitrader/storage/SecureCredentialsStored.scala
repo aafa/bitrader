b7aa6c2ecde7a0f979feb9cdf910892859ba0461
Alexey Afanasev <avafanasiev@gmail.com>
2016-05-20 13:14:58 +0300

    todo credentials

---
+++ src/main/scala/app/bitrader/storage/SecureCredentialsStored.scala
@@ -0,0 +1,12 @@
+package app.bitrader.storage
+
+import android.content.Context
+
+/**
+  * Created by Alex Afanasev
+  */
+trait SecureCredentialsStored {
+  def ctx : Context
+
+  // todo add android secure credentials storage
+}
